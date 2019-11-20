e0890b8868c9361b3e075d41c930e195255b29ca
diaoshizhe <654745845@qq.com>
2019-11-20 16:27:06 +0800

    logger

--- cluster_gcn.py
+++ cluster_gcn.py
@@ -164,12 +164,12 @@ def main(args):
             if j % args.log_every == 0:
                 print(f"epoch:{epoch}/{args.n_epochs}, Iteration {j}/"
                       f"{len(cluster_iterator)}:training loss", loss.item())
-                logger.info("epoch: %f, training loss: %f", epoch, loss.item())
+                logger.write("epoch: "+str(epoch) + "training loss: " + str(loss.item())+ "\n")
                 writer.add_scalar('train/loss', loss.item(),
                                   global_step=j + epoch * len(cluster_iterator))
         print("current memory:",
               torch.cuda.memory_allocated(device=pred.device) / 1024 / 1024)
-        logger.info("current memory", torch.cuda.memory_allocated(device=pred.device) / 1024 / 1024)
+        logger.write("current memory" + str(torch.cuda.memory_allocated(device=pred.device) / 1024 / 1024) + "\n")
 
         # evaluate
         if epoch % args.val_every == 0:
@@ -177,11 +177,11 @@ def main(args):
                 model, g, labels, val_mask, multitask)
             print(
                 "Val F1-mic{:.4f}, Val F1-mac{:.4f}". format(val_f1_mic, val_f1_mac))
-            logger.info("Val F1-mic: %f, Val F1-mac: %f", val_f1_mic, val_f1_mac)
+            logger.write("Val F1-mic: " + str(val_f1_mic) + "Val F1-mac: %f" + str(val_f1_mac)+ "\n")
             if val_f1_mic > best_f1:
                 best_f1 = val_f1_mic
                 print('new best val f1:', best_f1)
-                logger.info("new best val f1", best_f1)
+                logger.write("new best val f1" + str(best_f1)+ "\n")
                 torch.save(model.state_dict(), os.path.join(
                     log_dir, 'best_model.pkl'))
             writer.add_scalar('val/f1-mic', val_f1_mic, global_step=epoch)
@@ -189,7 +189,7 @@ def main(args):
 
     end_time = time.time()
     print(f'training using time {start_time-end_time}')
-    logger.info("training using time: %f", (start_time-end_time))
+    logger.write("training using time: " + str(start_time-end_time)+ "\n")
     # test
     if args.use_val:
         model.load_state_dict(torch.load(os.path.join(
@@ -197,7 +197,7 @@ def main(args):
     test_f1_mic, test_f1_mac = evaluate(
         model, g, labels, test_mask, multitask)
     print("Test F1-mic{:.4f}, Test F1-mac{:.4f}". format(test_f1_mic, test_f1_mac))
-    logger.info("Test F1-mic: %f, Test F1-mac: %f", test_f1_mic, test_f1_mac)
+    logger.write("Test F1-mic: " + str(test_f1_mic) + "Test F1-mac: %f" + str(test_f1_mac)+ "\n")
     writer.add_scalar('test/f1-mic', test_f1_mic)
     writer.add_scalar('test/f1-mac', test_f1_mac)
 
